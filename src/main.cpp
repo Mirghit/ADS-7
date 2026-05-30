7fa6b342d0121883fdb6b2d66eab10e6f4c846e9
Mirghit <yuandru25@gmail.com>
2026-05-30 16:11:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,48 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
 #include "train.h"
+#include "vector"
+#include "cstdlib"
+#include "ctime"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+  std::srand(static_cast<unsigned>(std::time(nullptr)));
+  const int MAX_LENGTH = 60;
+  std::vector<int> lengths;
+  std::vector<int> lightsOff;
+  std::vector<int> lightsOn;
+  std::vector<int> lightsChaotic;
 
-  while (count--)
-    train.addCar(false);
+  for (int i = 2; i <= MAX_LENGTH; ++i) {
+    Train trainLightsOff;
+    for (int j = 0; j < i; ++j) {
+      trainLightsOff.addCar(false);
+    }
+    trainLightsOff.getLength();
+    lightsOff.push_back(trainLightsOff.getOpCount());
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+    Train trainLightsOn;
+    for (int j = 0; j < i; ++j) {
+      trainLightsOn.addCar(true);
+    }
+    trainLightsOn.getLength();
+    lightsOn.push_back(trainLightsOn.getOpCount());
+
+    Train trainLightsChaotic;
+    for (int j = 0; j < i; ++j) {
+      trainLightsChaotic.addCar(std::rand() % 2 == 0);
+    }
+    trainLightsChaotic.getLength();
+    lightsChaotic.push_back(trainLightsChaotic.getOpCount());
+
+    lengths.push_back(i);
+  }
+  std::cout << "i, lights off, lights on, chaotic lights\n";
+  for(size_t i = 0; i < lengths.size(); ++i) {
+    std::cout << lengths[i] << ','
+      << lightsOff[i] << ','
+      << lightsOn[i] << ','
+      << lightsChaotic[i] << '\n';
+  }
   return 0;
 }
